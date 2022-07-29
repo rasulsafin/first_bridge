f819daf620ccef5aa8968d950301651ca6272f6f
rizmailov <yqmqZk5L>
2022-07-29 11:12:32 +0300

    fix axios.interceptor

--- DM/pwa-react/src/axios/axiosInstance.js
+++ DM/pwa-react/src/axios/axiosInstance.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-let headers = {};
-
-if (localStorage.token) {
-  headers.Authorization = `Bearer ${localStorage.token}`;
-}
-
 export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  headers,
 })
+
+axiosInstance.interceptors.request.use(request => {
+
+  const token = localStorage.getItem("token");
+  if (token)
+  {
+    request.headers.Authorization = `Bearer ${token}`;
+  }
+    return request;
+  });
