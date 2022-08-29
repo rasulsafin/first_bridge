a890c21fbb5c911d1f06786abaea6aaa1c89a56d
Хадиев Роберт <rhadiev@briogroup.ru>
2022-08-29 14:49:05 +0300

    "Added OrganizationEntity, new fields for entities: User, Project and Record"

---
+++ DM/DM.Domain/Interfaces/IOrganizationService.cs
@@ -0,0 +1,14 @@
+﻿using DM.DAL.Entities;
+using DM.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DM.Domain.Interfaces
+{
+    public interface IOrganizationService
+    {
+        public Task<List<OrganizationEntity>> GetAll();
+        public Task<OrganizationEntity> GetById(long organizationId);
+        public Task<bool> Create(OrganizationModel organizationModel);
+    }
+}
---
+++ DM/DM/Controllers/OrganizationController.cs
@@ -0,0 +1,38 @@
+﻿using DM.Domain.Interfaces;
+using DM.Domain.Models;
+using DM.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DM.Controllers
+{
+    [ApiController]
+    [Route("api/organization")]
+    public class OrganizationController : ControllerBase
+    {
+        private readonly IOrganizationService _organizationService;
+
+        public OrganizationController(IOrganizationService organizationService)
+        {
+            _organizationService = organizationService;
+        }
+
+        [Authorize(RoleConst.UserAdmin)]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var organizations = await _organizationService.GetAll();
+
+            return Ok(organizations);
+        }
+
+        //[Authorize(RoleConst.UserAdmin)]
+        [HttpPost]
+        public async Task<IActionResult> Create(OrganizationModel organizationModel)
+        {
+            var id = await _organizationService.Create(organizationModel);
+
+            return Ok(id);
+        }
+    }
+}
