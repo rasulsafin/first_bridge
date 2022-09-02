f95bac641da6384063a7dfb8c80557d03974e6c1
Хадиев Роберт <rhadiev@briogroup.ru>
2022-09-02 12:08:56 +0300

    Added flexible authorization and PermissionService, example in recordService. Reworked a lot of classes.

---
+++ DM/DM.Domain/Implementations/CurrentUserService.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using DM.Domain.Interfaces;
+using DM.Entities;
+using DM.repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace DM.Domain.Implementations
+{
+    public class CurrentUserService
+    {
+        private readonly DmDbContext _context;
+
+        public CurrentUserService(DmDbContext context)
+        {
+            _context = context;
+        }
+
+        public UserEntity CurrentUser { get; set; }
+
+        public async void SetCurrentUser(long userId)
+        {
+            CurrentUser = await _context.Users.FirstOrDefaultAsync(x => x.Id == userId);
+        }
+    }
+}
---
+++ DM/DM/Helpers/JwtHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace DM.Helpers
+{
+    public static class JwtHelper
+    {
+        public static long GetUserId(this HttpContext context)
+        {
+            return int.Parse(context.User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value);
+        }
+    }
+}
