a129daaa7f455b1b8839d56d50a35671cadd8bd4
Rustam Izmaylov <rizmailov@briogroup.ru>
2023-04-07 11:34:11 +0300

    auth request was moved from LoginPage to authSlice

--- DM/pwa-react/src/components/pages/LoginPage/LoginPage.js
+++ DM/pwa-react/src/components/pages/LoginPage/LoginPage.js
@@ -1,77 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Controls } from "../../controls/Controls";
 import "./LoginPage.css";
-import { useAuth } from "../../../hooks/useAuth";
 import { useDispatch } from "react-redux";
-import { axiosInstance } from "../../../axios/axiosInstance";
-import { setAuthUser } from "../../../services/authSlice";
+import { login } from "../../../services/authSlice";
 import { useNavigate } from "react-router";
 
 export const LoginPage = () => {
-  const { setAuth } = useAuth();
-  const navigate = useNavigate();
   const [user, setUser] = useState("");
   const [pwd, setPwd] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const [success, setSuccess] = useState(false);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    setErrMsg("");
-  }, [user, pwd]);
-  
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    try {
-      const response = await axiosInstance.post("api/users/authenticate",
-        JSON.stringify({ login: user, password: pwd }),
-        {
-          headers: { "Content-Type": "application/json-patch+json" },
-          withCredentials: true
-        }
-      );
 
-      dispatch(setAuthUser({
-        id: response.data.id,
-        name: response.data.name,
-        email: response.data.email,
-        token: response.data.token,
-        login: response.data.login,
-        role: response.data.role,
-        organizationId: response.data.organizationId
-      }));
-
-      console.log(JSON.stringify(response?.data));
-
-      const token = response?.data?.token;
-
-      // const roles = response?.data?.roles;
-
-      setUser("");
-      setPwd("");
-      setAuth({ user, token });
-      localStorage.setItem("token", response.data.token);
-      localStorage.setItem("user", response.data.name);
-      localStorage.setItem("role", response.data.role);
-      localStorage.setItem("organizationId", response.data.organizationId);
-      setSuccess(true);
-      navigate(`/`);
-    } catch (err) {
-      if (!err?.response) {
-        setErrMsg("No Server Response");
-      } else if (err.response?.status === 400) {
-        setErrMsg("Missing Username or Password");
-      } else if (err.response?.status === 401) {
-        setErrMsg("Unauthorized");
-      } else {
-        setErrMsg("Login Failed");
-      }
-    }
+    dispatch(login({ user, pwd })).then(() => {
+      navigate("/");
+    });
   };
-  
+
   return (
     <div className="Auth-form-container">
-      <form 
+      <form
         className="Auth-form"
         onSubmit={handleSubmit}
       >
