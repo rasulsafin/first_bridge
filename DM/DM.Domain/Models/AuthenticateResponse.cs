6a6d167287cf72e50b6e60777ceba9b37350f7e6
Rustam Izmaylov <rizmailov@briogroup.ru>
2023-04-13 14:10:47 +0300

    added profileForm

--- DM/DM.Domain/Models/AuthenticateResponse.cs
+++ DM/DM.Domain/Models/AuthenticateResponse.cs
@@ -6,21 +6,28 @@ namespace DM.Domain.Models
     {
         public long Id { get; set; }
         public string Name { get; set; }
+        public string LastName { get; set; }
+        public string FathersName { get; set; }
         public string Login { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
+        public string Password { get; set; }
+        public string Position { get; set; }
         public string Role { get; set; }
-        
         public long OrganizationId { get; set; }
 
         public AuthenticateResponse(UserEntity user, string token)
         {
             Id = user.Id;
             Name = user.Name;
+            LastName = user.LastName;
+            FathersName = user.FathersName;
             Login = user.Login;
             Email = user.Email;
             Role = user.Roles;
+            Position = user.Position;
             Token = token;
+            Password = user.Password;
             OrganizationId = user.OrganizationId;
         }
     }
--- DM/pwa-react/src/components/pages/ProfilePage/ProfilePage.js
+++ DM/pwa-react/src/components/pages/ProfilePage/ProfilePage.js
@@ -1,40 +1,28 @@
 import * as React from "react";
-import UserForm from "../UsersPage/components/UserForm";
-import { getUpdateInputDataFromValues } from "../UsersPage/utils/getUpdateInputDataFromValues";
 import { useDispatch, useSelector } from "react-redux";
-import { selectAllUsers } from "../../../services/usersSlice";
 import { logout, selectUser } from "../../../services/authSlice";
 import { Controls } from "../../controls/Controls";
 import { useNavigate } from "react-router";
+import ProfileForm from "./components/ProfileForm";
 
 export const ProfilePage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const textButton = "Save changes";
   const currentUser = useSelector(selectUser);
-  const users = useSelector(selectAllUsers);
-  const user = users.find(user => user.id === Number(currentUser.id));
-  const initialValues = getUpdateInputDataFromValues(user);
-  
+
   const handleLogout = () => {
     dispatch(logout());
     navigate(`/login`);
-  }
+  };
   
   return (
     <div className="component-container">
       <h3 className="mb-2">Профиль</h3>
-      <UserForm
-        textButton={textButton}
-        initialValues={initialValues}
-        onSubmit={(values, formikHelpers) => {
-          formikHelpers.resetForm();
-        }}
-      />
+      <ProfileForm user={currentUser} />
       <div
-      style={{
-        width: "200px"
-      }}
+        style={{
+          width: "200px"
+        }}
       >
         <Controls.Button
           className="ml-0 mt-3"
