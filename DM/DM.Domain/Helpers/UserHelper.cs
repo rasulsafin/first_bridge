d2602844add2925fbdfe4a778693fb43498ae95a
Александр Крайчик <akraychik@briogroup.ru>
2023-04-24 12:44:32 +0300

    update claims update auth response(deleted user)

--- DM/DM.Domain/Helpers/UserHelper.cs
+++ DM/DM.Domain/Helpers/UserHelper.cs
@@ -5,6 +5,9 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using DM.DAL.Entities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace DM.Domain.Helpers
 {
@@ -14,13 +17,25 @@ namespace DM.Domain.Helpers
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(configuration["Secret"]);
+
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()), new Claim("roleName", user.Role.Name) }),
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("id", user.Id.ToString()),
+                    new Claim("roleName", user.Role.Name),
+                    new Claim("userName", user.Name),
+                    new Claim("lastName", user.LastName),
+                    new Claim("fathersName", user.FathersName),
+                    new Claim("login", user.Login),
+                    new Claim("email", user.Email),
+                    new Claim("organizationId", user.OrganizationId.ToString()),
+                }),
+
                 Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new SigningCredentials(
-                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
+
             var token = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
         }
