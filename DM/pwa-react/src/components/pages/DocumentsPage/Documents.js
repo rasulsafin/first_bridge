efdde977d5ab1e03b6011f70551b16a4a65b04d1
Rustam Izmaylov <rizmailov@briogroup.ru>
2023-05-12 16:28:24 +0300

    fixed docs

--- DM/pwa-react/src/components/pages/DocumentsPage/Documents.js
+++ DM/pwa-react/src/components/pages/DocumentsPage/Documents.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Box } from "@mui/material";
 import { SearchBar } from "../../searchBar/SearchBar";
 
 export const Documents = () => {
   return (
     <div>
       <h3 className="mb-2">Документы</h3>
-      <Box>
+      <div className="toolbar-project">
         <SearchBar />
-      </Box>
+      </div>
     </div>
   );
 };
