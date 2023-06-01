6eaf0b5fa3b606c947cc67382c841142eaac8eb7
Rustam Izmaylov <rizmailov@briogroup.ru>
2023-06-01 17:59:19 +0300

    added appbar in modelPage, menu for element ifc and other fixed

--- DM/pwa-react/src/components/ifc/SideDrawer.js
+++ DM/pwa-react/src/components/ifc/SideDrawer.js
@@ -1,82 +1,84 @@
 import React, { useEffect, useState } from "react";
-import {useLocation} from 'react-router-dom'
-import Box from '@mui/material/Box'
-import Drawer from '@mui/material/Drawer'
-import {PropertiesPanel} from './SideDrawerPanel'
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import { PropertiesPanel } from "./SideDrawerPanel";
+import { useSelector } from "react-redux";
+import { isDrawerOpen, isDrawerOpenFromStore } from "../../services/controlUISlice";
 
-export function SideDrawer({isDrawerOpen, closeDrawer, isPropertiesOn}) {
+export function SideDrawer({ isDrawerOpen, closeDrawer, isPropertiesOn }) {
   useEffect(() => {
     if (!isPropertiesOn && isDrawerOpen) {
-      closeDrawer()
+      closeDrawer();
     }
-  }, [isPropertiesOn, isDrawerOpen, closeDrawer])
+  }, [isPropertiesOn, isDrawerOpen, closeDrawer]);
 
   return (
-    <>
-        <Drawer
-          open={isDrawerOpen}
-          anchor={'right'}
-          variant='persistent'
-          elevation={4}
-          sx={{
-            '&::-webkit-scrollbar': {
-              display: 'none',
-            },
-            '& > .MuiPaper-root': {
-              width: '450px',
-              // This lets the h1 in ItemProperties use 1em padding but have
-              // its mid-line align with the text in SearchBar
-              padding: '4px 1em',
-            },
-            '& .MuiPaper-root': {
-              marginTop: '0px',
-              borderRadius: '0px',
-            },
-          }}
+      <Drawer
+        open={isDrawerOpen}
+        anchor="right"
+        variant="persistent"
+        elevation={4}
+        sx={{
+          "&::-webkit-scrollbar": {
+            display: "none"
+          },
+          "& > .MuiPaper-root": {
+            width: "415px",
+            padding: "4px 1em"
+          },
+          "& .MuiPaper-root": {
+            marginTop: "0px",
+            borderRadius: "0px"
+          }
+        }}
+      >
+        <Box sx={{
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          overflowX: "hidden",
+          overflowY: "auto"
+        }}
         >
+          <Box
+            sx={{
+              display: "none",
+              height: isPropertiesOn ? "50%" : "100%",
+              borderRadius: "0px",
+              borderBottom: `1px solid #212529`,
+              paddingTop: "20px",
+              overflowX: "hidden",
+              overflowY: "auto"
+            }}
+          >
+          </Box>
           <Box sx={{
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            overflowX: 'hidden',
-            overflowY: 'auto',
+            display: isPropertiesOn ? "block" : "none",
+            height: "100%",
+            borderRadius: "5px",
+            overflowX: "hidden",
+            overflowY: "auto"
           }}
           >
-            <Box
-              sx={{
-                display: 'none',
-                height: isPropertiesOn ? '50%' : '100%',
-                borderRadius: '0px',
-                borderBottom: `1px solid #212529`,
-                paddingTop: '20px',
-                overflowX: 'hidden',
-                overflowY: 'auto',
-              }}
-            >
-            </Box>
-            <Box sx={{
-              display: isPropertiesOn ? 'block' : 'none',
-              height: '100%',
-              borderRadius: '5px',
-              overflowX: 'hidden',
-              overflowY: 'auto',
-            }}
-            >
-              {isPropertiesOn && <PropertiesPanel/>}
-            </Box>
+            {isPropertiesOn && <PropertiesPanel />}
           </Box>
-        </Drawer>
-    </>
-  )
+        </Box>
+      </Drawer>
+  );
 }
 
 export default function SideDrawerWrapper() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(true)
-  const [closeDrawer, setCloseDrawer] = useState(false)
-  const [isPropertiesOn, setIsPropertiesOn] = useState(true)
-  const [openDrawer, setOpenDrawer] = useState(true)
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [closeDrawer, setCloseDrawer] = useState(false);
+  const [isPropertiesOn, setIsPropertiesOn] = useState(true);
+  const [openDrawer, setOpenDrawer] = useState(true);
+  const isOpen = useSelector(isDrawerOpenFromStore);
 
+  useEffect(() => {
+    setIsDrawerOpen(isOpen)
+  }, [isOpen])
+  
   return (
     <>
       {isDrawerOpen &&
@@ -87,5 +89,5 @@ export default function SideDrawerWrapper() {
           openDrawer={openDrawer}
         />}
     </>
-  )
+  );
 }
--- DM/pwa-react/src/services/controlUISlice.js
+++ DM/pwa-react/src/services/controlUISlice.js
@@ -1,21 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isUserModalOpen: false
+  isModalOpen: false,
+  isDrawerOpen: false
 };
 
 export const controlUISlice = createSlice({
   name: "controlUI",
   initialState,
   reducers: {
-    toggleUserModal(state) {
+    toggleModal(state) {
       state.isUserModalOpen = !state.isUserModalOpen;
+    },
+    toggleDrawer(state) {
+      state.isDrawerOpen = !state.isDrawerOpen;
+    },
+    openDrawer(state) {
+      state.isDrawerOpen = true;
     }
   }
 });
 
-export const { toggleUserModal } = controlUISlice.actions;
+export const { toggleDrawer, openDrawer } = controlUISlice.actions;
 
-export const selectUserModalOpen = (state) => state.controlUI.isUserModalOpen;
+export const isModalOpen = (state) => state.controlUI.isModalOpen;
+export const isDrawerOpenFromStore = (state) => state.controlUI.isDrawerOpen;
 
 export const controlUIReducer = controlUISlice.reducer;
