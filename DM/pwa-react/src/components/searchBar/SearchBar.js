3af3bc1fffe2df673aaf3d5b4d1468ccea5f8f34
Rustam Izmaylov <rizmailov@briogroup.ru>
2023-06-07 11:29:16 +0300

    added filter button to searchbar

--- DM/pwa-react/src/components/searchBar/SearchBar.js
+++ DM/pwa-react/src/components/searchBar/SearchBar.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { InputAdornment, TextField } from "@mui/material";
+import { Badge, IconButton, InputAdornment, TextField, Typography } from "@mui/material";
 import { ReactComponent as SearchIcon } from "../../assets/icons/search.svg";
+import { ReactComponent as FilterIcon } from "../../assets/icons/filter.svg";
 import "./SearchBar.css";
+import { useDispatch } from "react-redux";
+import { toggleDrawer } from "../../services/controlUISlice";
 
 export const SearchBar = (props) => {
-  const { onChange } = props;
+  const { onChange, filter } = props;
+  const dispatch = useDispatch();
 
   return (
-    <div style={{ width: "100%" }}>
+    <div style={{ width: "100%", display: "flex", flexDirection: "row" }}>
       <TextField
         className="search-input"
         placeholder="Поиск"
@@ -40,16 +44,60 @@ export const SearchBar = (props) => {
             >
               <SearchIcon />
             </InputAdornment>
-          )
-          // endAdornment: value && (
-          //   <Button 
-          //     onClick={() => setValue("")}
-          //   >
-          //     <CancelIcon />
-          //   </Button>
-          // )
+          ),
+          endAdornment:
+            <InputAdornment
+              position="end"
+              style={{
+                marginRight: "20px"
+              }}
+            >
+            </InputAdornment>
         }}
       />
+      {filter &&
+        <IconButton
+          sx={{
+            backgroundColor: "#FFF",
+            height: "36px",
+            borderTopRightRadius: "5px",
+            borderBottomRightRadius: "5px",
+            borderTopLeftRadius: 0,
+            borderBottomLeftRadius: 0,
+            paddingRight: "18px"
+          }}
+          onClick={() => dispatch(toggleDrawer())}
+        >
+          <FilterIcon
+            style={{
+              marginRight: "5px"
+            }}
+          />
+          <Badge
+            badgeContent={2}
+            color="error"
+            sx={{
+              "& .MuiBadge-badge":
+                {
+                  right: -9,
+                  top: 10,
+                  fontSize: 12,
+                  padding: 0,
+                  width: 15,
+                  border: "3px solid #FFF"
+                }
+            }}
+          >
+            <Typography
+              sx={{
+                fontWeight: "bold",
+                fontFamily: "Myriad Pro"
+              }}
+            >Фильтр
+            </Typography>
+          </Badge>
+        </IconButton>
+      }
     </div>
   );
 };
