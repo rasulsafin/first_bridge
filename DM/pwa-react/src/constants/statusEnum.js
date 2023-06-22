dd77a4fe5d26ade14c572d22149b22d7efabc2e6
Rustam Izmaylov <rizmailov@briogroup.ru>
2023-06-22 12:35:34 +0300

    updated recordCard

---
+++ DM/pwa-react/src/constants/statusEnum.js
@@ -0,0 +1,8 @@
+export const statusEnum = [
+  { id: 1, title: "В работе", color: "#283BE8" },
+  { id: 2, title: "Не определена", color: "#a9acc2" },
+  { id: 3, title: "Открыта", color: "#d2052b" },
+  { id: 4, title: "Готова", color: "#35c206"},
+  { id: 5, title: "Перенесена", color: "#8451d0" },
+  { id: 6, title: "Закрыта", color: "#060709" }
+];
