ea65542ed9d5f3ae768b75e1ede5ba7ae87e2ad4
Rustam Izmaylov <rizmailov@briogroup.ru>
2023-06-22 16:39:28 +0300

    fixed delete project dialog

--- DM/pwa-react/src/components/pages/ProjectsPage/components/ProjectDeleteDialog.js
+++ DM/pwa-react/src/components/pages/ProjectsPage/components/ProjectDeleteDialog.js
@@ -1,36 +1,59 @@
 import React from "react";
-import { Dialog, Grid } from "@mui/material";
+import { Dialog, IconButton } from "@mui/material";
 import { Controls } from "../../../controls/Controls";
+import { ReactComponent as CancelIcon } from "../../../../assets/icons/cancel.svg";
+import { reduceTitle } from "../utils/reduceTitle";
 
 export const ProjectDeleteDialog = (props) => {
   const { project, handleDeleteProject, open, close } = props;
 
   return (
     <Dialog
-      PaperProps={{ sx: { position: "fixed", bottom: 50, left: "35vw", maxWidth: "md", m: 0, padding: 2 } }}
+      PaperProps={{
+        sx: {
+          borderRadius: "10px",
+          minWidth: "40vw",
+          height: "58px",
+          position: "fixed",
+          bottom: "16px",
+          left: "30vw"
+        }
+      }}
       open={open}
       onClose={close}
       hideBackdrop
     >
-      <Grid container>
-        <Grid item md={12}>
-          <span>
-           {`Вы действительно хотите удалить проект ${project.title} ?`}
-          </span>
-          <Controls.Button
-            onClick={handleDeleteProject}
-            variant="outlined"
-            color="error"
-          >Да</Controls.Button>
-          <Controls.Button
-            onClick={close}
-            variant="outlined"
-            autoFocus
-          >
-            Нет
-          </Controls.Button>
-        </Grid>
-      </Grid>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "58px"
+        }}
+      >
+        {`Вы действительно хотите удалить проект ${reduceTitle(project.title)}?`}
+        <Controls.Button
+          onClick={handleDeleteProject}
+          style={{
+            backgroundColor: "#2D2926",
+            color: "#FFF",
+            border: "none"
+          }}
+        >Да</Controls.Button>
+        <Controls.Button
+          onClick={close}
+          variant="outlined"
+          autoFocus
+        >
+          Нет
+        </Controls.Button>
+        <IconButton
+          onClick={close}
+        >
+          <CancelIcon />
+        </IconButton>
+      </div>
     </Dialog>
   );
 };
